f2b0aff54e47a541bdf3fe9acbf61de2b51ac489
Graham Heath <Graham.Heath@rga.com>
2014-12-16 11:42:41 -0600

    watch jsx, but not post.js (jsx outputs *.post.js)

--- gulp/watch.js
+++ gulp/watch.js
@@ -5,7 +5,7 @@ var gulp = require('gulp');
 gulp.task('watch', ['styles'] ,function () {
   console.log('Indigo Painted Hunting Dog');
   gulp.watch('src/{app,components}/**/*.scss', ['styles']);
-  gulp.watch('src/{app,components}/**/*.js', ['scripts','jsx']);
+  gulp.watch(['src/{app,components}/**/*.js','!src/{app,components}/**/*.post.js'], ['scripts','jsx']);
   gulp.watch('src/assets/images/**/*', ['images']);
   gulp.watch('bower.json', ['wiredep']);
 });
