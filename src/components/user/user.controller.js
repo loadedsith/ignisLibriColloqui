5da8de6ce1099783af4e227945222cfb8ac7e393
Graham Heath <Graham.Heath@rga.com>
2014-12-19 15:25:27 -0600

    add user image to nav bar

--- src/components/user/user.controller.js
+++ src/components/user/user.controller.js
@@ -4,8 +4,18 @@ angular.module('ignisLibriColloqui')
   .controller('UserController', ['$scope','UserService', function ($scope, UserService) {
 
     console.log('UserController- UserService:', UserService);
-    
+    $scope.debugUser = false;
     $scope.checkLoginState = UserService.checkLoginState;
+    
+    $scope.userInitialized = function () {
+      if($scope.user() === undefined){
+        return false;
+      }
+      if($scope.user().id === undefined){
+        return false;
+      }
+      return true;
+    }
     
     $scope.loginToFacebook = UserService.loginToFacebook;
     
