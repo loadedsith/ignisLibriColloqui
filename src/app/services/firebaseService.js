e83700186054220517004a161b97fcec70250a51
Graham Heath <Graham.Heath@rga.com>
2015-01-05 16:21:22 -0600

    clean up

--- src/app/services/firebaseService.js
+++ src/app/services/firebaseService.js
@@ -5,5 +5,6 @@ angular.module('ignisLibriColloqui.Firebase',[])
     firebaseService.matchesRef = new Firebase('https://resplendent-fire-9421.firebaseIO.com/interests');
     firebaseService.chatRef = new Firebase('https://resplendent-fire-9421.firebaseIO.com/chats');
     firebaseService.usersRef = new Firebase('https://resplendent-fire-9421.firebaseIO.com/users');
+  
     return firebaseService;
   });
