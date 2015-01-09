fc892c1c6ac00d387a8b1aafabc8216377c589fb
Graham Heath <Graham.Heath@rga.com>
2015-01-09 11:54:00 -0600

    /src/app/env.post.js -> /tmp/app/env.js

--- gulp/myEnv.js
+++ gulp/myEnv.js
@@ -9,8 +9,8 @@ var env = require('../env.json');
 //inject env var
 gulp.task('myEnv', function () {
   return gulp.src('./src/app/env.pre.js')
-    .pipe(rename('env.post.js'))
+    .pipe(rename('env.js'))
     .pipe(preprocess({context: {FB_API_KEY: env.FB_API_KEY}}))
-    .pipe(gulp.dest('src/app'));
+    .pipe(gulp.dest('.tmp/app'));
 });
 
