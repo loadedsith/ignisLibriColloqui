489482687c37f743de4c64ab52108155ebea727c
Graham Heath <Graham.Heath@rga.com>
2015-01-09 15:57:03 -0600

    duplicate linter (couldn't get js and jsx to both be linted)

--- gulp/build.js
+++ gulp/build.js
@@ -36,7 +36,25 @@ notify.on('click', function (options) {
 });
 
 gulp.task('scripts', function() {
-  gulp.src('src/{app,components}/**/*.js')
+  gulp.src(['src/{app,components}/**/*.js'])
+    .pipe($.jshint())
+    // Use gulp-notify as jshint reporter
+    .pipe(notify({
+      title: 'JSHint',
+      message:function (file) {
+      if (file.jshint.success) {
+        // Don't show something if success
+        return false;
+      }
+      var errors = file.jshint.results.map(function (data) {
+        if (data.error) {
+          return '(' + data.error.line + ':' + data.error.character + ') ' + data.error.reason+'\n'+
+             'txmt://open?url=file://' + file.path + '&line='+data.error.line + '&column=' + data.error.character+'\n';
+        }
+      }).join('\n');
+      return file.relative + ' (' + file.jshint.results.length + ' errors)\n' + errors;
+    },wait:true}));
+  gulp.src(['src/{app,components}/**/*.jsx'])
     .pipe($.jshint())
     // Use gulp-notify as jshint reporter
     .pipe(notify({
