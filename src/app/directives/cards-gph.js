2bbb92aa67def5ad7bfcba1b2a9d488c1aa52d61
Graham Heath <Graham.Heath@rga.com>
2015-01-10 16:51:09 -0600

    clean up, allow cards template to be passed on card directive, supports react modules only

--- src/app/directives/cards-gph.js
+++ src/app/directives/cards-gph.js
@@ -9,37 +9,43 @@ define(['/app/directives/directiveModule.js', '/app/react/cards.js', 'react'], f
           initialPosition: '=',
           duration: '=',
           swipeRight: '=',
-          swipeLeft: '='
-          
+          swipeLeft: '=',
+          cardTemplate: '='
         },
         link: function(scope, el) {//extraAttr: attrs 
-          var MyComponent = React.createFactory(CARDS);
+          var Cards = React.createFactory(CARDS);
           var maxDrag = scope.maxDrag;
           var duration = scope.duration;
           var initialPosition = scope.initialPosition;
+          var cardTemplate = scope.cardTemplate;
           var swipeRight;
           var swipeLeft;
 
           if(typeof scope.swipeRight === 'function'){
             swipeRight = scope.swipeRight;
           }else{
-            swipeRight = function () {console.log('defualt swipeRight event');};
+            swipeRight = function (card) {
+              console.log('defualt swipeRight event', card);
+            };
           }
 
           if(typeof scope.swipeLeft === 'function'){
             swipeLeft = scope.swipeLeft;
           }else{
-            swipeLeft = function (card) {console.log('defualt swipeLeft event');};
+            swipeLeft = function (card) {
+              console.log('defualt swipeLeft event', card);
+            };
           }
           
           scope.$watch('cards', function(newValue) {//extraAttr: oldValue
-            React.render(new MyComponent({
+            React.render(new Cards({
               data: newValue,
               maxDrag: maxDrag,
               duration: duration,
               initialPosition: initialPosition,
               swipeRight: swipeRight,
-              swipeLeft: swipeLeft
+              swipeLeft: swipeLeft,
+              cardTemplate:cardTemplate
             }), el[0]);
 
           }, true);
