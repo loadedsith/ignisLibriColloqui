5758a8a9ec64df6b2e9ac69c842fe1f979a02e4b
Graham Heath <Graham.Heath@rga.com>
2015-01-10 16:50:15 -0600

    update matchCards directive template for new directives

--- src/components/matchCards/matchCards.controller.js
+++ src/components/matchCards/matchCards.controller.js
@@ -1,13 +1,40 @@
 define(['controllerModule', 'angular'],function (controllers) {
   'use strict';
-  return controllers.controller('MatchCardsController', ['$scope', '$timeout', 'UserService','FacebookService', function ($scope, $timeout, UserService, FacebookService) {
+  return controllers.controller('MatchCardsController', ['$scope', '$timeout', 'UserService','FacebookService', function ($scope, $timeout, UserService, facebookService) {
     console.log('Hi everybody, im the MatchCardsController');
+    $scope.matchlist = {};
     $scope.matches = function () {
       if ( UserService.matches === undefined){
-        return [];
+        return undefined;
       }
+      
+      var matches = UserService.matches[UserService.currentTopic];
+      
+      for (var i = matches.length - 1; i >= 0; i--) {
+        var match = matches[i];
+        if($scope.matchlist[match] === undefined) {
+          $scope.matchlist[match] = {
+            facebookId:match
+          }
+        }
+        
+        if($scope.matchlist[match].fetching === undefined) {
+          $scope.matchlist[match].fetching = true;
+          $scope.matchlist[match].image = facebookService.getUserImageById(match, function(image){
+            $scope.matchlist[match].fetching = 'fetched';//lol
+            $scope.matchlist[match].image=image;
+          });
+        }
+        /*
+        {
+        userId:match,
+        image:
+        }
+        */
 
-      return UserService.matches[UserService.currentTopic];
+      }
+      // return undefined;
+      return $scope.matchlist;
     };
     
     $scope.getUserImageById = function (id) {
@@ -20,7 +47,7 @@ define(['controllerModule', 'angular'],function (controllers) {
         //set a loading image, prevents this from getting called while the image is being loaded
         $scope.images[id]='http://placehold.it/50x50';
         //get the actual image, will be set when callback is fired
-        FacebookService.getUserImageById(id, function (imageUrl) {
+        facebookService.getUserImageById(id, function (imageUrl) {
           if (imageUrl.error === undefined){
             $timeout(function () {
               $scope.images[id] = imageUrl.data.url;
@@ -35,13 +62,26 @@ define(['controllerModule', 'angular'],function (controllers) {
 
     };
     
-    $scope.swipeLeft = function (eventName, $event) {
-      console.log('swipeLeft: eventName, $event', eventName, $event);
-    };
+    $scope.swipeLeft = function (card) {
+       console.log('swipeLeft: card', card, $scope, $scope.cards);
+       var myScope = $scope;
+       card.returnCard();
 
-    $scope.swipeRight = function (eventName, $event) {
-      console.log('swipeRight: eventName, $event', eventName, $event);
-    };
+     };
+     $scope.removeCard = function (card) {
+       console.log('removeCard');
+       $timeout(function () {
+         $scope.matches.splice($scope.matches.indexOf(card),1);
+       },0);
+     };
+     $scope.swipeRight = function (card) {
+       console.log('swipeRight: card', card, $scope, $scope.cards);
+       var myScope = $scope;
+       card.fadeOut(function (card) {
+         console.log('Green wooden Blue Whale');
+         myScope.removeCard(card.getDOMNode().innerHTML);
+       });
+     };
     
     $scope.date = new Date();
     
