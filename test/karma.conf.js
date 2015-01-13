3cbf68e3954205e43f2ecd2eeb27a2132252d23d
Graham Heath <Graham.Heath@rga.com>
2015-01-13 14:39:02 -0600

    clean up karma.conf.js

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -3,46 +3,42 @@
 module.exports = function(config) {
 
   config.set({
-    basePath : '..', //!\\ Ignored through gulp-karma //!\\
+    basePath : '..',
 
-    files : [ //!\\ Ignored through gulp-karma //!\\
-        // 'src/bower_components/angular/angular.js',
-        // 'src/bower_components/angular/angular-route.js',
-        // 'src/bower_components/angular-mocks/angular-mocks.js',
-        // {pattern: 'node_modules/**/*.js', included: false},
+    files : [
         {pattern: 'bower_components/**/*.js', included: false},
         {pattern: 'src/{app,components}/**/*.js', included: false},
         {pattern: '.tmp/{app,components}/**/*.js', included: false},
         {pattern: 'test/**/*spec.js', included: false},
-        // 'src/{app,components}/** /*.js',
-        // 'test/unit/** /*.js',
-        'test/require.config.test.js'
+        'test/require.config.test.js',
+        'test/reactPolyfillForPhantomJS.js'
     ],
     exclude : [
-      // 'node_modules/**/*.js',
       'src/app/env.pre.js',
       'src/app/main.js'
     ],
     autoWatch : false,
-    browserNoActivityTimeout: 100000,
-    
+    // browserNoActivityTimeout: 100000,
     // logLevel: config.LOG_DEBUG,
+    singleRun:false,
 
     frameworks: ['jasmine','requirejs'],
 
     preprocessors: {
       '**/*.jsx': ['react']
     },
-// you can define custom flags
-    // customLaunchers: {
-      // Chrome_without_security: {
-      //   base: 'Chrome',
-      //   flags: ['--disable-web-security']
-      // }
-    // },
-    browsers: ['Chrome'],//'Chrome_without_security'
-
-    hostname:'example.com',//example.com has been added to etc/hosts as the localhost, it is also registered with the app on the Facebook API 
+    // you can define custom flags
+    /* customLaunchers: {
+       Chrome_without_security: {
+         base: 'Chrome',
+         flags: ['--disable-web-security']
+       }
+     },*/
+    browsers: ['PhantomJS'],//'Chrome','Chrome_without_security'
+      
+    //example.com has been added to etc/hosts as the localhost,
+    //it is also registered with the app on the Facebook API
+    hostname:'example.com',
 
     plugins : [
         'karma-phantomjs-launcher',
