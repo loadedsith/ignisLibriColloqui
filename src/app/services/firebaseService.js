904f491c13f2cca1fb19d0bba6af9b721cd4fb52
Graham Heath <Graham.Heath@rga.com>
2015-01-14 09:50:57 -0600

    support Config.firebaseUrl in firebaseService

--- src/app/services/firebaseService.js
+++ src/app/services/firebaseService.js
@@ -2,9 +2,17 @@ define(['services/serviceModule', 'angular', 'firebase'], function (services, an
   'use strict';
   return services.service('FirebaseService', function (Config) {
       var firebaseService = this;
-      firebaseService.matchesRef = new Firebase('https://resplendent-fire-9421.firebaseIO.com/interests');
-      firebaseService.messagesRef = new Firebase('https://resplendent-fire-9421.firebaseIO.com/messages');
-      firebaseService.usersRef = new Firebase('https://resplendent-fire-9421.firebaseIO.com/users');
+
+      if (Config.firebaseUrl) {
+        firebaseService.firebaseUrl = Config.firebaseUrl;
+      }else{
+        console.log('Strings.firebaseUrl was unset');
+        firebaseService.firebaseUrl = 'https://resplendent-fire-9421.firebaseIO.com';
+      }
+
+      firebaseService.matchesRef = new Firebase(firebaseService.firebaseUrl + '/interests');
+      firebaseService.messagesRef = new Firebase(firebaseService.firebaseUrl + '/messages');
+      firebaseService.usersRef = new Firebase(firebaseService.firebaseUrl + '/users');
        
       return firebaseService;
     });  
