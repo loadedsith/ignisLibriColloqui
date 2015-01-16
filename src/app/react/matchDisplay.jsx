c98d9b93147c738cb35f9bceeae330de4f5425e8
Graham Heath <Graham.Heath@rga.com>
2015-01-16 12:47:52 -0600

    make cards look better, .card .matchDisplay has its own css!

--- src/app/react/matchDisplay.jsx
+++ src/app/react/matchDisplay.jsx
@@ -1,21 +1,26 @@
 define(['react'], function(React) {
   'use strict';
   return React.createClass({
+   
     render: function() {
       var imageUrl;
+      
       //This is essentailly the same as if(___===undefined){} blocks. getting this.props.data.image.data
       var imageData = (((this.props.data||{}).image||{}).data||{});
-      if(imageData.is_silhouette === true){
+      
+      if(imageData['is_silhouette'] === true){
         imageUrl = './assets/images/FBProfile.jpg';
       }else{
         imageUrl = imageData.url||'./assets/images/FBProfile.jpg';
       }
-      
       /*jshint ignore:start */
+      var styles = {
+        backgroundImage:'url(' + imageUrl + ')'
+      };
       /* <p>FBID:{this.props.data.facebookId}</p>*/
       /** <p>fetching:{this.props.data.fetching}</p>*/
-      return <div>
-        <img src={imageUrl} className="matchImage"/>
+      // <img src={imageUrl} className="matchImage"/>
+      return <div className="matchDisplay" style={styles}>
       </div>
       /*jshint ignore:end */
       
