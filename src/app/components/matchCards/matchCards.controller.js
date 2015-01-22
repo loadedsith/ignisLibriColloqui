9cf52e03f96cb5db4620233190e2d02211c5251a
Graham Heath <Graham.Heath@rga.com>
2015-01-21 21:54:29 -0600

    capitalize matchList, bypass processMatches (temporarily)

--- src/app/components/matchCards/matchCards.controller.js
+++ src/app/components/matchCards/matchCards.controller.js
@@ -4,12 +4,12 @@ define(['controllerModule', 'angular', 'react/matchDisplay'], function(controlle
   ['$scope', '$timeout', 'UserService', 'FacebookService',
   function($scope, $timeout, UserService, facebookService) {
     console.log('Hi everybody, im the MatchCardsController');
-    $scope.matchlist = {};
+    $scope.matchList = {};
     $scope.MatchDisplay = MatchDisplay;
 
     var imageMatchLookup = function(id, image) {
-      var matches = $scope.matchlist;
-      var matchesKeys = Object.keys($scope.matchlist);
+      var matches = $scope.matchList;
+      var matchesKeys = Object.keys($scope.matchList);
       for (var i = matchesKeys.length - 1; i >= 0; i--) {
         var match = matchesKeys[i];
         if (matches[match].facebookId === id) {
@@ -22,8 +22,9 @@ define(['controllerModule', 'angular', 'react/matchDisplay'], function(controlle
 
     $scope.$on('UserService:Update', function(event, user) {
       if (user.matches) {
-        $scope.processMatches(user.matches);
-        console.log('$scope.matchlist', $scope.matchlist);
+        // $scope.processMatches(user.matches);//TODO: re-implement processMatches
+        $scope.matchList = user.matches;
+        console.log('$scope.matchList', $scope.matchList);
       } else {
         console.debug('got service update for match controller, but no matches!')
       }
@@ -43,20 +44,20 @@ define(['controllerModule', 'angular', 'react/matchDisplay'], function(controlle
 
       for (var i = matches.length - 1; i >= 0; i--) {
         var match = matches[i];
-        if ($scope.matchlist[match] === undefined) {
-          $scope.matchlist[match] = {
+        if ($scope.matchList[match] === undefined) {
+          $scope.matchList[match] = {
             facebookId:match
           };
         }
 
-        if ($scope.matchlist[match].fetching === undefined) {
-          $scope.matchlist[match].fetching = true;
-          $scope.matchlist[match].image =
+        if ($scope.matchList[match].fetching === undefined) {
+          $scope.matchList[match].fetching = true;
+          $scope.matchList[match].image =
             facebookService.getUserImageById(match, facebookImageConfig, imageMatchLookup);
         }
 
       }
-      return $scope.matchlist;
+      return $scope.matchList;
     };
 
     $scope.getUserImageById = function(id) {
@@ -87,7 +88,7 @@ define(['controllerModule', 'angular', 'react/matchDisplay'], function(controlle
     $scope.removeCard = function(card) {
       console.log('removeCard');
       $timeout(function() {
-        delete $scope.matchlist[card.facebookId];
+        delete $scope.matchList[card.facebookId];
       }, 0);
     };
     $scope.cardControl = {
