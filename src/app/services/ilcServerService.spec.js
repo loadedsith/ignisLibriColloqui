be4c0328498fb86c8dc3e604e9b3104573b9d68b
Graham Heath <Graham.Heath@rga.com>
2015-02-03 14:11:02 -0600

    ilcServerService tests, only on its listening to socket events.

--- src/app/services/ilcServerService.spec.js
+++ src/app/services/ilcServerService.spec.js
@@ -4,17 +4,53 @@
 define(['services/serviceModule', 'angular-mocks'], function() {
   describe('ILC Server Services', function() {
     var ilcServerService;
-
+    var scope;
+    var socket;
+    var events = [
+      'got user matchList',
+      'message sent',
+      'pong',
+      'room set',
+      'room update',
+      'rooms set',
+      'rooms update',
+      'user disconnected',
+      'user profile',
+    ]
     beforeEach(module('ignisLibriColloqui.services'));
+
+    beforeEach(inject(function($socket) {
+      socket = $socket;
+      spyOn(socket,'on')
+    }));
 
     beforeEach(inject(function(ILCServerService) {
       ilcServerService = ILCServerService;
     }));
 
-    describe(' ilcServerService ', function() {
-      it(' should execute normally', function() {
+
+     describe('ilcServerService', function() {
+      it('should execute normally', function() {
         expect(true);// just by getting here you've verified the callback (it was done())
       });
+      it('should have a server url', function() {
+        expect(ilcServerService.ilcServerUrl).toBeDefined();
+      });
+      it('should have an instance of $socket', function() {
+        expect(socket).toBeDefined();
+      });
+    });
+    describe('has many socket connections', function() {
+      console.log('socket', socket);
+      it('at least one on call', function() {
+        expect(socket.on).toHaveBeenCalled();
+      });
+      for (var i = events.length - 1; i >= 0; i--) {
+        var event = events[i];
+        it('should listen for event['+i+']: on(\'' + event + '\')', function() {
+          expect(socket.on).toHaveBeenCalledWith(event, jasmine.any(Function));
+        });
+      }
     });
 
   });
