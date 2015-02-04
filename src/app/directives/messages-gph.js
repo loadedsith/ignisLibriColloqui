5e4cd6b9f8612f1553150ea64c6b96757bea090c
Graham Heath <Graham.Heath@rga.com>
2015-02-03 21:46:29 -0600

    silence messages-gph before local user has an id

--- src/app/directives/messages-gph.js
+++ src/app/directives/messages-gph.js
@@ -10,7 +10,13 @@ define(['directives/directiveModule', 'react/messages', 'react'], function(direc
       link: function(scope, el) {//extraAttr: attrs
         var messagesFactory = React.createFactory(Messages);
         scope.$watch('messages', function(newValue) {//extraAttr: oldValue
-          newValue.localUser = scope.localUser;
+          if(newValue){
+            if(scope.localUser){
+              newValue.localUser = scope.localUser;
+            }else{
+              newValue.localUser = 'there\'s no user name bob.';
+            }
+          }
           React.render(new messagesFactory({data: newValue}), el[0]);
         }, true);
       }
