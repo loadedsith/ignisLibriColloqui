2ab1d8f485153fef05428dfc9537e4d59976c2f8
Graham Heath <Graham.Heath@rga.com>
2015-02-05 17:24:30 -0600

    give up on live testing, use watch to retrigger Karma from the base

--- gulp/unit-tests.js
+++ gulp/unit-tests.js
@@ -7,14 +7,22 @@ var $ = require('gulp-load-plugins')();
 var karma = require('karma').server;
 var karmaRunner = require('karma').runner;
 
-gulp.task('test', ['myEnv','test-server'], function(done) {
-  karmaRunner.run({
+gulp.task('test', ['myEnv','karmaTeam'], function(done) {
+  gulp.watch('src/{app,components}/**/*.spec.js', ['karmaTeam']);
+  // gulp.watch('src/{app,components}/**/*.spec.js', ['karmaRunner']);
+});
+
+gulp.task('karmaTeam', function(done) {
+  karma.start({
     configFile: __dirname + '/../test/karma.conf.js',
-  },function() {
-    karma.start({
-      configFile: __dirname + '/../test/karma.conf.js',
-      singleRun: false,
-      autoWatch:true
-    });
+    singleRun: true,
+    autoWatch:false
+  },done);
+})
+gulp.task('karmaRunner', function(done) {
+  karma.start({
+    configFile: __dirname + '/../test/karma.conf.js',
+    singleRun: false,
+    autoWatch:true
   });
 });
