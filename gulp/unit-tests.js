3bbab882600d51a51aaca333ed206a99df8fb6f8
Graham Heath <Graham.Heath@rga.com>
2015-02-09 12:09:05 -0600

    tdd for gulp/unit-test.js

--- gulp/unit-tests.js
+++ gulp/unit-tests.js
@@ -7,8 +7,9 @@ var $ = require('gulp-load-plugins')();
 var karma = require('karma').server;
 var karmaRunner = require('karma').runner;
 
+// you could the 'test-server' task here, but I think its nicer to have in a seperate terminal
 gulp.task('test', ['myEnv','karmaTeam'], function(done) {
-  gulp.watch('src/{app,components}/**/*.spec.js', ['karmaTeam']);
+  gulp.watch('src/{app,components}/**/*.js', ['karmaTeam']);
   // gulp.watch('src/{app,components}/**/*.spec.js', ['karmaRunner']);
 });
 
@@ -16,8 +17,8 @@ gulp.task('karmaTeam', function(done) {
   karma.start({
     configFile: __dirname + '/../test/karma.conf.js',
     singleRun: true
-  },function(a,b,c) {
-    console.log('karma is down');
+  },function() {
+    console.log('restarted!')
     done();
   });
 })
