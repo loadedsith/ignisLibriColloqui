64918d6741d4169c244b971b31e8fc9ecc05d4cc
Graham Heath <Graham.Heath@rga.com>
2015-02-11 14:34:54 -0600

    disable browsersync notifications

--- gulp/server.js
+++ gulp/server.js
@@ -27,6 +27,7 @@ function browserSyncInit(baseDir, files, browser) {
       middleware: middleware,
       routes: routes
     },
+    notify:false,
     ghostMode: false,
     open:false,
     browser: browser,
