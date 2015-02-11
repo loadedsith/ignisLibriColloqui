e7e9bef8b15a96dd6dd8f83b296cc0102a39a405
Graham Heath <Graham.Heath@rga.com>
2015-02-11 10:03:45 -0600

    messages-gph don't try and append an object to an array

--- src/app/directives/messages-gph.js
+++ src/app/directives/messages-gph.js
@@ -10,14 +10,7 @@ define(['directives/directiveModule', 'react/messages', 'react'], function(direc
       link: function(scope, el) {//extraAttr: attrs
         var messagesFactory = React.createFactory(Messages);
         scope.$watch('messages', function(newValue) {//extraAttr: oldValue
-          if(newValue){
-            if(scope.localUser){
-              newValue.localUser = scope.localUser;
-            }else{
-              newValue.localUser = 'there\'s no user name bob.';
-            }
-          }
-          React.render(new messagesFactory({data: newValue}), el[0]);
+          React.render(new messagesFactory({data: newValue,localUser:scope.localUser}), el[0]);
         }, true);
       }
     };
