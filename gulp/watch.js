0d5c3a0b856d5e6e818415529eda7e682c24f6e3
Graham Heath <Graham.Heath@rga.com>
2015-03-02 15:15:39 -0600

    requirejsBuild is optional for watch tasks (for example, while working on require builds)

--- gulp/watch.js
+++ gulp/watch.js
@@ -4,7 +4,7 @@ var gulp = require('gulp');
 
 gulp.task('watch', ['scripts','styles'] , function() {
   gulp.watch('src/app/**/*.scss', ['styles']);
-  gulp.watch(['src/app/**/*.js','src/vendor/**/*.js'], ['scripts', 'requirejsBuild']);
+  gulp.watch(['src/app/**/*.js','src/vendor/**/*.js'], ['scripts']);
   gulp.watch(['src/app/**/*.jsx'], ['jsx', 'scripts']);
   gulp.watch('src/assets/images/**/*', ['images']);
   gulp.watch(['env.json', 'src/app/env.pre.js'], ['myEnv']);
